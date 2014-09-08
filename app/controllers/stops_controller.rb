248dc806ae42575cdf880e741dc0df08dc378345
Christian Danielsen & Patrick David <student@epicodus.com>
2014-09-08 15:51:23 -0700

    add update/destroy action to stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -18,4 +18,25 @@ class StopsController < ApplicationController
   def show
     @stop = Stop.find(params[:id])
   end
+
+  def edit
+    @stop = Stop.find(params[:id])
+  end
+
+  def update
+    @stop = Stop.find(params[:id])
+    if @stop.update(params[:stop])
+      flash[:notice]="Stop updated"
+      redirect_to("/stops/#{@stop.id}")
+    else
+      render('edit')
+    end
+  end
+
+  def destroy
+    @stop = Stop.find(params[:id])
+    @stop.destroy
+    flash[:alert]="Stop Deleted"
+    redirect_to('/stops')
+  end
 end
