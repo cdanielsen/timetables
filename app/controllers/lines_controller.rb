6dbe429eb63bbdd9625bdfcfde6175509031f637
Christian Danielsen & Patrick David <student@epicodus.com>
2014-09-08 15:10:49 -0700

    add edit/update/destroy methods to line class

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -14,4 +14,30 @@ class LinesController < ApplicationController
       render('index')
     end
   end
+
+  def show
+    @line = Line.find(params[:id])
+  end
+
+  def edit
+    @line =Line.find(params[:id])
+  end
+
+  def update
+    @line = Line.find(params[:id])
+    if @line.update(params[:line])
+      flash[:notice]="Line Updated"
+      redirect_to("/lines/#{@line.id}")
+    else
+      render('edit')
+    end
+  end
+
+  def destroy
+    @line = Line.find(params[:id])
+    @line.destroy
+    flash[:alert]= "Line Deleted"
+    redirect_to('/lines')
+  end
+
 end
